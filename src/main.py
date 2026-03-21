040c3b079beb502dea32c7f10512916b771f3f7e
natalia <natsil220119@gmail.com>
2026-03-21 17:53:03 -0500

    corrige ruta

--- src/main.py
+++ src/main.py
@@ -9,18 +9,25 @@ from models.usuario import Usuario
 
 class Servidor(BaseHTTPRequestHandler):
 
-    def responder(self, data):
-        self.send_response(200)
+    def responder(self, data, status=200):
+        self.send_response(status)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(data).encode())
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
 
     def do_GET(self):
         parsed = urlparse(self.path)
         ruta = parsed.path
         params = parse_qs(parsed.query)
 
-        if ruta == "/insertar":
+        if ruta == "/":
+            self.responder({"mensaje": "API funcionando. Usa /insertar"})
+
+        elif ruta == "/insertar":
             try:
                 id = int(params["id"][0])
                 nombre = params["nombre"][0]
@@ -32,10 +39,10 @@ class Servidor(BaseHTTPRequestHandler):
                 self.responder({"mensaje": "Usuario guardado"})
 
             except Exception as e:
-                self.responder({"error": str(e)})
+                self.responder({"error": str(e)}, 500)
 
         else:
-            self.responder({"mensaje": "Ruta válida: /insertar"})
+            self.responder({"error": "Ruta no encontrada"}, 404)
 
 
 if __name__ == "__main__":
