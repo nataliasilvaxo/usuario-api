3487c7cb9bf13e8e586a3c712bd03db844771a82
natalia <natsil220119@gmail.com>
2026-03-21 21:02:28 -0500

    test 3

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
 from flask import Flask, jsonify, request
 from conexion import coleccion
 from models.usuario import Usuario
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return jsonify({
-        "ruta": request.path,
-        "query": request.query_string.decode()
-    })
+    id_usuario = request.args.get("id")
+    nombre = request.args.get("nombre")
+    correo = request.args.get("correo")
 
+    # 👉 Si vienen datos → guarda en Mongo
+    if id_usuario and nombre and correo:
+        try:
+            usuario = Usuario(int(id_usuario), nombre, correo)
+            coleccion.insert_one(usuario.to_dict())
+            return jsonify({"mensaje": "Usuario guardado"})
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+    # 👉 Si no vienen datos → solo prueba API
+    return jsonify({"mensaje": "API funcionando"})
+
+# 👇 IMPORTANTE para Render
 if __name__ == "_main_":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
