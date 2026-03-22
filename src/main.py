1403107f8812bdc2c676b51b844166bdff461683
natalia <natsil220119@gmail.com>
2026-03-21 20:00:51 -0500

    ruta raiz

--- src/main.py
+++ src/main.py
@@ -3,23 +3,21 @@ from conexion import coleccion
 from models.usuario import Usuario
 import os
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def inicio():
-    return jsonify({"mensaje": "API funcionando"})
-
-@app.route("/insertar", strict_slashes=False)
-def insertar():
-    id_usuario = int(request.args.get("id"))
+    id_usuario = request.args.get("id")
     nombre = request.args.get("nombre")
     correo = request.args.get("correo")
 
-    usuario = Usuario(id_usuario, nombre, correo)
-    coleccion.insert_one(usuario.to_dict())
+    if id_usuario and nombre and correo:
+        usuario = Usuario(int(id_usuario), nombre, correo)
+        coleccion.insert_one(usuario.to_dict())
+        return jsonify({"mensaje": "Usuario guardado"})
 
-    return jsonify({"mensaje": "Usuario guardado"})
+    return jsonify({"mensaje": "API funcionando"})
 
-if __name__ == "_main_":
+if _name_ == "_main_":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
