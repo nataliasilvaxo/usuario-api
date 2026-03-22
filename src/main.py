1434fb7455b8d00e5609761edec1939dc2061b8c
natalia <natsil220119@gmail.com>
2026-03-21 19:45:50 -0500

    flask insertar puerto

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from conexion import coleccion
 from models.usuario import Usuario
+import os
 
 app = Flask(__name__)
 
@@ -18,3 +19,7 @@ def insertar():
     coleccion.insert_one(usuario.to_dict())
 
     return jsonify({"mensaje": "Usuario guardado"})
+
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
